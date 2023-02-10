fce59c2ebd4d8f65c4ffd46c39e76f91c8b987de
utopiasz <108901659+utopiasz@users.noreply.github.com>
2023-02-10 17:42:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,5 +7,28 @@ function main(element){
         element.style.backgroundColor = "rgb(171, 205, 238)";
     }
 }
+/* 
+function main(element){
+    var currentColor = element.style.backgroundColor;
+    if (currentColor == "rgb(171, 205, 238)"){
+        element.style.backgroundColor = "rgb(166, 245, 189)";
+    }
+    else{
+        element.style.backgroundColor = "rgb(171, 205, 238)";
+    }
+}
+*/ 
 
-    
+function ShowTimes() {
+    var now = new Date();
+    var hrs = 23-now.getHours();
+    var mins = 59-now.getMinutes();
+    var secs = 59-now.getSeconds();
+    var str = '';
+        str += ''+hrs+':'+mins+':'+secs;
+    document.getElementById('countdownToMidnight').innerHTML = str;
+  }
+  var _cntDown;
+  function StopTimes() {
+      clearInterval(_cntDown);
+  }
