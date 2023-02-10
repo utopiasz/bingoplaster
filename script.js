41dad1bcf0bf97a5d69af85de4276378c920e8b3
utopiasz <108901659+utopiasz@users.noreply.github.com>
2023-02-10 17:05:03 +0100

    demo version

---
+++ script.js
@@ -0,0 +1,11 @@
+function main(element){
+    var currentColor = element.style.backgroundColor;
+    if (currentColor == "rgb(171, 205, 238)"){
+        element.style.backgroundColor = "rgb(166, 245, 189)";
+    }
+    else{
+        element.style.backgroundColor = "rgb(171, 205, 238)";
+    }
+}
+
+    
