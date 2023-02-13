5d5a4a2512e238edd7008f420573cae770677d8e
utopiasz <108901659+utopiasz@users.noreply.github.com>
2023-02-13 01:11:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-function main(element){
+function main(element){ // najszybciej będzie po prostu dać tego inwersję (negację)
     var currentColor = element.style.backgroundColor;
-    if (currentColor == "rgb(171, 205, 238)"){
-        element.style.backgroundColor = "rgb(166, 245, 189)";
-    }
-    else{
-        element.style.backgroundColor = "rgb(171, 205, 238)";
-    }
+    element.style.backgroundColor = currentColor !== "rgb(166, 245, 189)" ? "rgb(166, 245, 189)" : "rgb(171, 205, 238)";
 }
 /* 
 function main(element){
